5682cd35bf83c24ff24775beeaf71566cc981ae2
piletskiy <piletskiy10@gmail.com>
2021-12-19 20:02:20 +0200

    work in progress

--- src/js/components/RoomObj.js
+++ src/js/components/RoomObj.js
@@ -5,7 +5,7 @@ import socket from "../socket";
 export default function RoomObj({room}) {
     let history = useHistory();
     let imageStyleObj = {
-        backgroundImage: room.otherUser.avatar ? "url(" + process.env.BACKEND_URL + "/" + room.otherUser.avatar + ")" : "url('./build/assets/defaultAvatar.png')"
+        backgroundImage: room.otherUser?.avatar ? "url(" + process.env.BACKEND_URL + "/" + room.otherUser.avatar + ")" : "url('./build/assets/defaultAvatar.png')"
     }
     return (
         <div
@@ -20,9 +20,9 @@ export default function RoomObj({room}) {
             <div className="roomClass-avatar" style={imageStyleObj}>
             </div>
             <div className="roomClass-info">
-                <p className="room-user-name">{room.otherUser.name + " " + room.otherUser.lastName}</p>
-                <p className="room-last-message">{room.lastMessage?.messageObj.message}</p>
-                <p className="room-last-message-time">{room.lastMessage?.createdAt}</p>
+                <p className="room-user-name">{room?.otherUser?.name + " " + room.otherUser?.lastName}</p>
+                <p className="room-last-message">{room?.lastMessage?.messageObj.message}</p>
+                <p className="room-last-message-time">{room?.lastMessage?.createdAt}</p>
             </div>
             <div className="clear">
             </div>
